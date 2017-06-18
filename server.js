ffba389272dc09b3dfec170aa100daa4bc9c76b7
Gabriel Angelus <gangeluscf@gmail.com>
2017-06-18 14:14:23 -0300

    Criando form de notificação

--- server.js
+++ server.js
@@ -46,6 +46,12 @@ var emitir = function(req, res, next){
 app.listen(port);
 
 app.use(emitir);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api', router);
 
 router.route('/notificar')
