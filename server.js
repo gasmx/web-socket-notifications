51661db7c93bce18781851bbf5006e10dd72fb31
Gabriel Angelus <gangeluscf@gmail.com>
2017-06-18 13:08:07 -0300

    Ajustando tipos de notificação

--- server.js
+++ server.js
@@ -1,28 +1,58 @@
-var express = require('express')
-  , app = express()
-  , server = require('http').createServer(app).listen(4555)
-  , io = require('socket.io').listen(server)
-  , bodyParser = require('body-parser');
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-  var port = process.env.PORT || 8080;
-  var router = express.Router();
-  /* Socket irá aqui depois */
-  var emitir = function(req, res, next){
-	var notificar = req.query.notificacao || '';
-		if(notificar != '')	 {
-		io.emit('notificacao', notificar);
-		next();
-	} else {
-			next();
-		}
-	}
-  app.use(emitir);
-  app.use('/api', router);
-  router.route('/notificar')
-    .get(function(req, res){
-    //aqui vamos receber a mensagem
-    res.json({message: "testando essa rota"})
-    })
-  app.listen(port);
-  console.log('conectado a porta ' + port);
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app).listen(4555),
+    io = require('socket.io').listen(server),
+    bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+var router = express.Router();
+
+var emitir = function(req, res, next){
+
+  var msgDeNotificacao = 'nao definido';
+
+  if (typeof req.query.info != 'undefined') {
+    msgDeNotificacao = {
+      'type': 'info',
+      'msg': req.query.info
+    };
+  }
+
+  if (typeof req.query.error != 'undefined') {
+    msgDeNotificacao = {
+      'type': 'error',
+      'msg': req.query.error
+    }
+  }
+
+  if (typeof req.query.success != 'undefined') {
+    msgDeNotificacao = {
+      'type': 'success',
+      'msg': req.query.success
+    }
+  }
+
+  if(msgDeNotificacao != '')	 {
+  	io.emit('notificacao', msgDeNotificacao);
+  	next();
+  } else {
+    next();
+  }
+}
+
+app.listen(port);
+
+app.use(emitir);
+app.use('/api', router);
+
+router.route('/notificar')
+  .get(function(req, res){
+  //aqui vamos receber a mensagem
+  res.json({message: "testando essa rota"})
+  });
+
+
+console.log('Conectado a porta ' + port);
